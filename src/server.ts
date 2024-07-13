52f9520b378a3b1f69f2fbd78cfc30b10693a5a4
João Victor <victor.jvsb18@gmail.com>
2024-07-13 21:16:47 +0000

    update host listen

--- src/server.ts
+++ src/server.ts
@@ -62,6 +62,6 @@ app.register(getTripDetails)
 app.register(getParticipant)
 
 const PORT = env.PORT || 3333;
-app.listen({ port: PORT }).then(() => {
+app.listen({ port: PORT, host: '0.0.0.0' }).then(() => {
   console.log(`Server is running on ${PORT}`)
 })
